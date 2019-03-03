8db243991bc1b0061359dd06930a3ecaf8567d61
James Muspratt <james@jamesmuspratt.com>
2019-03-02 22:06:24 -0500

    Rough setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <ul class="cards">
+      <li v-for="animal in animals">
+        <Card :file="animal"/>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
+
+const animals = [
+      'elephant',
+      'lion',
+      'fox',
+      'tiger',
+      'rabbit',
+      'owl'
+      ];
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+const dups = animals.concat(animals);
+const dupsShuffled = shuffle(dups);
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      animals: dupsShuffled
+    }
   }
 }
+
+ 
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+body {
+  padding: 0;
+  margin: 0;
 }
+#app {
+  background: #eee;
+  margin: 0;
+  padding: 40px;
+}
+.cards {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: grid;
+  // flex-wrap: wrap;
+  // justify-content: flex-start;
+    grid-gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  // grid-gap: 20px;
+  // justify-items: center;
+  // align-items: end;
+  // width: 100%;
+}
+
+li {
+  max-width: 100%;
+  display: block;
+  // width: 25%;
+  // margin: 10px;
+
+}
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card" @click="flip" >
+    <img :src="url" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    file: String
+  },
+  data() {
+    return {
+      url: '/' + this.file + '.png',
+    }
+  },
+  methods: {
+    flip($event) {
+      $event.target.classList.toggle('flipped')
+      this.$emit('flipped')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+* {
+  box-sizing: border-box;
+}
+.card {
+  border: 1px solid #ddd;
+  border-radius: 15px;
+   &.flipped {
+    border: 1px solid red;
+    }
+}
+  img {
+    display: block;
+    max-width: 100%;
+  
+  pointer-events: none;
+  }
+
+</style>
